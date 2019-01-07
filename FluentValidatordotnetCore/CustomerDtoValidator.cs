6d7b6cc7534dc4191e5c6d186b0dd24012d1ebca
Jucinei <jucineisantos@outlook.com>
2019-01-07 11:32:25 -0200

    Criacao Projeto

---
+++ FluentValidatordotnetCore/CustomerDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using FluentValidatordotnetCore.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FluentValidatordotnetCore
+{
+    public class CustomerDtoValidator : AbstractValidator<Customer>
+    {
+        public CustomerDtoValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("O nome é obrigatório");
+            RuleFor(x => x.Name).MaximumLength(100).WithMessage("O tamanho máximo é de 100 caracteres");
+            RuleFor(x => x.Name).MinimumLength(10).WithMessage("O tamanho minimo é de 10 caracteres");
+
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Email invalido");
+        }
+    }
+}
---
+++ FluentValidatordotnetCore/Program.cs
@@ -0,0 +1,57 @@
+﻿using FluentValidatordotnetCore.Model;
+using System;
+using System.Linq;
+
+namespace FluentValidatordotnetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+
+
+            AddCustomerSimple(customer);
+
+
+            AddCustomerWithFluentValidation(customer);
+
+            Console.ReadKey();
+
+        }
+
+        /// <summary>
+        /// Validação utilizando FluentValidation
+        /// </summary>
+        /// <param name="customer"></param>
+        static void AddCustomerWithFluentValidation(Customer customer)
+        {
+            var validator = new CustomerDtoValidator();
+            var answerValidation = validator.Validate(customer);
+            if (!answerValidation.IsValid)
+            {
+                Console.WriteLine(answerValidation.Errors.FirstOrDefault());
+            }
+        }
+
+        /// <summary>
+        /// Simple way to Validate Fields
+        /// Forma simples para validação de campos
+        /// </summary>
+        /// <param name="customer"></param>
+        static void AddCustomerSimple(Customer customer)
+        {
+            if (!string.IsNullOrEmpty(customer.Name))
+            {
+                Console.WriteLine("O nome campo é obrigatório");
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(customer.Email))
+            {
+                Console.WriteLine("O nome email é obrigatório");
+                return;
+            }
+        }
+    }
+}
